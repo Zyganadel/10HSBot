c33813ad220c35d70abb535b25722fa12f3b68bf
Zyganadel <121130526+Zyganadel@users.noreply.github.com>
2025-02-01 21:48:32 -0400

    Partially setup request stuff.

--- 10HSBot/_10HSBot.py
+++ 10HSBot/_10HSBot.py
@@ -1,9 +1,12 @@
 import discord
 import discord.ext.commands
+import requests
 
 
 intents = discord.Intents.default();
 intents.message_content = True;
+
+version = '0.0.1';
 
 dsToken = input('Input 10hs bot token');
 inraToken = input('Input inara token');
@@ -27,8 +30,11 @@ async def test(ctx:discord.ext.commands.Context, message: str):
 
 @bot.hybrid_command(name='link', with_app_command=True)
 async def test(ctx:discord.ext.commands.Context, username: str):
+    headers={'appName':'10HSBot','appVersion':version,'APIkey':inraToken,'isBeingDeveloped':True};
+    params={};
+    y = requests.get('https://inara.cz/inapi/v1/', headers=headers);
     print(username);
     await ctx.send('This command is not implemented yet.');
     pass;
 
-bot.run(dsToken)
+bot.run(dsToken);
